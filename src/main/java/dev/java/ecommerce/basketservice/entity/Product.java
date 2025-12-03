601a84cede238aee0af192536f647d360ffa92cc
Emilianno <emidudickel@gmail.com>
2025-12-03 00:30:10 -0300

    feat: criado entity Product

--- src/main/java/dev/java/ecommerce/basketservice/entity/Product.java
+++ src/main/java/dev/java/ecommerce/basketservice/entity/Product.java
@@ -1,9 +1,19 @@
 package dev.java.ecommerce.basketservice.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 
 import java.math.BigDecimal;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Product {
     @Id
     private Long id;
