15b57625c2855622a4628a7f8a8e4e18b4e01d09
Emilianno <emidudickel@gmail.com>
2025-12-03 23:14:51 -0300

    feat: criado api para alterar carrinho, alterar metodo de pagamento e tambem de deletar carrinho.

--- src/main/java/dev/java/ecommerce/basketservice/service/BasketService.java
+++ src/main/java/dev/java/ecommerce/basketservice/service/BasketService.java
@@ -2,12 +2,12 @@ package dev.java.ecommerce.basketservice.service;
 
 import dev.java.ecommerce.basketservice.client.response.PlatziProductResponse;
 import dev.java.ecommerce.basketservice.controller.request.BasketRequest;
+import dev.java.ecommerce.basketservice.controller.request.PaymentRequest;
 import dev.java.ecommerce.basketservice.entity.Basket;
 import dev.java.ecommerce.basketservice.entity.Product;
 import dev.java.ecommerce.basketservice.entity.Status;
 import dev.java.ecommerce.basketservice.repository.BasketRepository;
 import lombok.RequiredArgsConstructor;
-import org.jspecify.annotations.Nullable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -20,7 +20,7 @@ public class BasketService {
     private final BasketRepository basketRepository;
     private final ProductService productService;
 
-    public Basket createBasket(BasketRequest basketRequest){
+    public Basket createBasket(BasketRequest basketRequest) {
 
         basketRepository.findByClientAndStatus(basketRequest.clientId(), Status.OPEN)
                 .ifPresent(basket -> {
@@ -47,4 +47,50 @@ public class BasketService {
         return basketRepository.findById(id)
                 .orElseThrow(() -> new IllegalStateException("Carrinho não encontrado"));
     }
+
+    public Basket updateBasket(String id, BasketRequest basketRequest) {
+        Basket basket = getBasketById(id);
+
+        if (basket.getStatus() == Status.SOLD) {
+            throw new IllegalStateException("Não é possível atualizar um carrinho fechado");
+        }
+
+        List<Product> products = new ArrayList<>();
+        basketRequest.products().forEach(productRequest -> {
+            PlatziProductResponse platziProductResponse = productService.getProductId(productRequest.productID());
+            products.add(Product.builder()
+                    .id(platziProductResponse.id())
+                    .title(platziProductResponse.title())
+                    .price(platziProductResponse.price())
+                    .quantity(productRequest.quantity())
+                    .build());
+        });
+
+        basket.setProducts(products);
+        basket.CalculateTotalPrice();
+        return basketRepository.save(basket);
+    }
+
+    public Basket payBasket(String basketId, PaymentRequest request) {
+        Basket basket = getBasketById(basketId);
+
+        if (basket.getStatus() == Status.SOLD) {
+            throw new IllegalStateException("Não é possível pagar um carrinho fechado");
+        }
+
+        basket.setStatus(Status.SOLD);
+        if (request != null) {
+            basket.setPaymentMethod(request.getPaymentMethod());
+        }
+        return basketRepository.save(basket);
+    }
+
+    public void deleteBasket(String basketId) {
+        var basket = getBasketById(basketId);
+        if (basket == null){
+            throw new IllegalStateException("Carrinho não encontrado!");
+        }
+        basketRepository.deleteById(basketId);
+    }
+
 }
