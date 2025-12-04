1a60b66880e96fb724e742ba7f628861743ea163
Emilianno <emidudickel@gmail.com>
2025-12-03 23:11:35 -0300

    feat: adicionado método de pagamento e tambem anotacao jsoinclude para remover valor null do novo atributo metodo de pagamento

--- src/main/java/dev/java/ecommerce/basketservice/entity/Basket.java
+++ src/main/java/dev/java/ecommerce/basketservice/entity/Basket.java
@@ -1,5 +1,11 @@
 package dev.java.ecommerce.basketservice.entity;
-import lombok.*;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -7,8 +13,8 @@ import java.math.BigDecimal;
 import java.util.List;
 
 
-
-@Document(collection = "basket")// anotação para dizer que essa classe é um documento do mongoDB
+@Document(collection = "basket")
+// anotação para dizer que essa classe é um documento do mongoDB
 @Setter
 @Getter
 @NoArgsConstructor
@@ -21,8 +27,10 @@ public class Basket {
     private BigDecimal totalPrice;
     private List<Product> products;
     private Status status;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private PaymentMethod paymentMethod;
 
-    public void CalculateTotalPrice (){
+    public void CalculateTotalPrice() {
         this.totalPrice = products.stream().map(product -> product.getPrice().multiply(BigDecimal.valueOf(product.getQuantity())))
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
